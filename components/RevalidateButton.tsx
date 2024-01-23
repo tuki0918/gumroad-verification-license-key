7289ba2c9d248e629320533d8b8cd7060024f4de
Yuta Yamamoto <tuki0918@gmail.com>
2024-01-24 00:30:45 +0900

    Add RevalidateButton component

---
+++ components/RevalidateButton.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { FC } from "react";
+
+const RevalidateButton: FC<{ code: string }> = ({ code }) => {
+  const router = useRouter();
+  const handleRevalidate = async (code: string) => {
+    try {
+      const res = await fetch("http://localhost:3000/api/licenses/revalidate", {
+        method: "POST",
+        body: JSON.stringify({
+          license_key: code,
+        }),
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+      if (res.ok) {
+        router.refresh();
+      } else {
+        console.log("Oops! Something is wrong.");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <button
+      className="btn btn-ghost btn-xs text-red-600"
+      onClick={() => void handleRevalidate(code)}
+    >
+      ●
+    </button>
+  );
+};
+
+export default RevalidateButton;
