f7d7ccc50086f66c05d363dd372cd6a5ba456997
Yuta Yamamoto <tuki0918@gmail.com>
2024-01-24 00:27:17 +0900

    Add functions to verify license and fetch subscription data from Gumroad API

--- types/gumroad.d.ts
+++ types/gumroad.d.ts
@@ -6,13 +6,14 @@ type Card = {
   expiry_year: null;
 };
 
-type Purchase = {
+export type Purchase = {
   seller_id: string;
   product_id: string;
   product_name: string;
   permalink: string;
   product_permalink: string;
   short_product_id: string;
+  /** @deprecated WARNING */
   email: string;
   price: number;
   gumroad_fee: number;
@@ -21,6 +22,7 @@ type Purchase = {
   discover_fee_charged: boolean;
   can_contact: boolean;
   referrer: string;
+  /** @deprecated WARNING */
   card: Card;
   order_number: number;
   sale_id: string;
@@ -38,10 +40,40 @@ type Purchase = {
   dispute_won: boolean;
   id: string;
   created_at: string;
-  custom_fields: any[];
+  custom_fields: {
+    discord_grant_role?: string;
+  };
   subscription_ended_at: string | null;
   subscription_cancelled_at: string | null;
   subscription_failed_at: string | null;
+};
+
+type SubscriberStatus =
+  | "alive"
+  | "pending_cancellation"
+  | "pending_failure"
+  | "failed_payment"
+  | "fixed_subscription_period_ended"
+  | "cancelled";
+
+export type Subscriber = {
+  id: string;
+  product_id: string;
+  product_name: string;
+  user_id: string;
+  /** @deprecated WARNING */
+  user_email: string;
+  purchase_ids: string[];
+  created_at: string;
+  user_requested_cancellation_at: string | null;
+  charge_occurrence_count: number | null;
+  recurrence: string;
+  cancelled_at: string | null;
+  ended_at: string | null;
+  failed_at: string | null;
+  free_trial_ends_at: string | null;
+  license_key: string;
+  status: SubscriberStatus;
 };
 
 type ApiResponseSuccess<T> = T & {
@@ -58,4 +90,8 @@ type ApiResponse<T> = ApiResponseSuccess<T> | ApiResponseError;
 export type ApiVerifyLicenseResponse = ApiResponse<{
   uses: number;
   purchase: Purchase;
+}>;
+
+export type ApiSubscriptionResponse = ApiResponse<{
+  subscriber: Subscriber;
 }>;
--- utils/gumroad.ts
+++ utils/gumroad.ts
@@ -1,16 +1,33 @@
-import type { ApiVerifyLicenseResponse } from "@/types/gumroad";
+import type {
+  ApiSubscriptionResponse,
+  ApiVerifyLicenseResponse,
+} from "@/types/gumroad";
+import { z } from "zod";
 
-export const verifyLicense = async (
-  product_id: string,
-  license_key: string,
-) => {
+const licenseKeySchema = z
+  .string()
+  .regex(/^[A-F0-9]{8}-[A-F0-9]{8}-[A-F0-9]{8}-[A-F0-9]{8}$/i);
+export const isLicenseKeyFormat = (code: string): boolean => {
+  try {
+    licenseKeySchema.parse(code);
+    return true;
+  } catch (e) {
+    console.error("Invalid license key format:", e);
+    return false;
+  }
+};
+
+export const verifyLicense = async (productId: string, licenseKey: string) => {
   const endpoint = "https://api.gumroad.com/v2/licenses/verify";
   const res = await fetch(endpoint, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ product_id, license_key }),
+    body: JSON.stringify({
+      product_id: productId,
+      license_key: licenseKey,
+    }),
   });
   const data = await res.json();
   if (!res.ok) {
@@ -18,4 +35,22 @@ export const verifyLicense = async (
     throw new Error("Failed to fetch data");
   }
   return data as Promise<ApiVerifyLicenseResponse>;
+};
+
+export const fetchSubscription = async (subscriptionId: string) => {
+  const params = new URLSearchParams();
+  params.append("access_token", process.env.GUMROAD_ACCESS_TOKEN || "");
+  const endpoint = `https://api.gumroad.com/v2/subscribers/${subscriptionId}?${params.toString()}`;
+  const res = await fetch(endpoint, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await res.json();
+  if (!res.ok) {
+    console.error(data);
+    throw new Error("Failed to fetch data");
+  }
+  return data as Promise<ApiSubscriptionResponse>;
 };
