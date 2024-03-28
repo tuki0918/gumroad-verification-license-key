4004423e9cf60adb63c674cbc01bbfbe6231c215
Yuta Yamamoto <tuki0918@gmail.com>
2024-03-28 18:25:51 +0900

    Add globalThis.prismaGlobal to manage Prisma client instance

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -1,3 +1,14 @@
+// https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices
 import { PrismaClient } from "@prisma/client";
 
-export const client = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+export const client = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = client;
