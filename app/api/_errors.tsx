43aa461863187a7809d0940e66064f10dac08a9a
Yuta Yamamoto <tuki0918@gmail.com>
2024-03-28 14:36:38 +0900

    Refactor error classes to use readonly code property

--- app/api/_errors.tsx
+++ app/api/_errors.tsx
@@ -1,53 +1,52 @@
-export class CustomError extends Error {
-  code: string;
-  constructor(message: string, code: string) {
+export abstract class CustomError extends Error {
+  /** HTTP status code */
+  readonly status: number = 500;
+  /** Error code */
+  readonly code: string = "CUSTOM_ERROR";
+  constructor(message: string) {
     super(message);
-    this.code = code;
+    this.name = this.constructor.name;
   }
 }
 
-export const ERROR_CODE__INVALID_LICENSE_KEY = "INVALID_LICENSE_KEY";
-export const ERROR_CODE__LICENSE_KEY_ALREADY_EXISTS =
-  "LICENSE_KEY_ALREADY_EXISTS";
-export const ERROR_CODE__LICENSE_KEY_DISABLED = "LICENSE_KEY_DISABLED";
-export const ERROR_CODE__FAILED_TO_VERIFY_LICENSE_KEY =
-  "FAILED_TO_VERIFY_LICENSE_KEY";
-export const ERROR_CODE__SUBSCRIPTION_IS_NOT_ALIVE =
-  "SUBSCRIPTION_IS_NOT_ALIVE";
-export const ERROR_CODE__SUBSCRIPTION_NOT_FOUND = "SUBSCRIPTION_NOT_FOUND";
-
 export class InvalidLicenseKeyError extends CustomError {
+  readonly code: string = "INVALID_LICENSE_KEY";
   constructor(message: string) {
-    super(message, ERROR_CODE__INVALID_LICENSE_KEY);
+    super(message);
   }
 }
 
 export class LicenseKeyAlreadyExistsError extends CustomError {
+  readonly code: string = "LICENSE_KEY_ALREADY_EXISTS";
   constructor(message: string) {
-    super(message, ERROR_CODE__LICENSE_KEY_ALREADY_EXISTS);
+    super(message);
   }
 }
 
 export class LicenseKeyDisabledError extends CustomError {
+  readonly code: string = "LICENSE_KEY_DISABLED";
   constructor(message: string) {
-    super(message, ERROR_CODE__LICENSE_KEY_DISABLED);
+    super(message);
   }
 }
 
 export class FailedToVerifyLicenseKeyError extends CustomError {
+  readonly code: string = "FAILED_TO_VERIFY_LICENSE_KEY";
   constructor(message: string) {
-    super(message, ERROR_CODE__FAILED_TO_VERIFY_LICENSE_KEY);
+    super(message);
   }
 }
 
 export class SubscriptionIsNotAliveError extends CustomError {
+  readonly code: string = "SUBSCRIPTION_IS_NOT_ALIVE";
   constructor(message: string) {
-    super(message, ERROR_CODE__SUBSCRIPTION_IS_NOT_ALIVE);
+    super(message);
   }
 }
 
 export class SubscriptionNotFoundError extends CustomError {
+  readonly code: string = "SUBSCRIPTION_NOT_FOUND";
   constructor(message: string) {
-    super(message, ERROR_CODE__SUBSCRIPTION_NOT_FOUND);
+    super(message);
   }
 }
