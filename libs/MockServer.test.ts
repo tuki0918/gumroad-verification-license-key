2827c9739b09e4a24c35057602adc7503c4fd136
Yuta Yamamoto <tuki0918@gmail.com>
2024-04-27 19:58:39 +0900

    Add mock server setup with testResponseMock2 override

--- libs/MockServer.test.ts
+++ libs/MockServer.test.ts
@@ -1,4 +1,6 @@
-import { testResponseMock } from "../mocks/mocks";
+import { testResponseMock } from "@/mocks/mocks";
+import { server } from "@/mocks/node";
+import { http, HttpResponse } from "msw";
 
 describe("MockServer", () => {
   it("should return a valid testResponseMock when request successful", async () => {
@@ -13,5 +15,30 @@ describe("MockServer", () => {
     });
     const response = await res.json();
     expect(response).toStrictEqual(testResponseMock);
+  });
+
+  it("should return a valid testResponseMock when request successful - mock override", async () => {
+    const testResponseMock2 = {
+      ...testResponseMock,
+      message: "Test response mock override",
+    } satisfies typeof testResponseMock;
+
+    server.use(
+      http.get("https://example.com/test", () => {
+        return HttpResponse.json(testResponseMock2);
+      }),
+    );
+
+    const endpoint = "https://example.com/test";
+    const res = await fetch(endpoint, {
+      // method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // body: JSON.stringify({
+      // }),
+    });
+    const response = await res.json();
+    expect(response).toStrictEqual(testResponseMock2);
   });
 });
